3224c378dccb9857a2f437e5aba9dc8c06a9b3e2
Malik Abu-Kalokoh <malik.abukalokoh@yahoo.com>
2017-12-26 19:37:20 -0500

    Created Login Page

--- PythonApp/pythonapp.py
+++ PythonApp/pythonapp.py
@@ -1,7 +1,12 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect \
+    ,render_template, request, session, abort
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html',name="Malik")
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
