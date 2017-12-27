98710418833034bec0c2995729ea8360a6a97ee1
Malik Abu-Kalokoh <malik.abukalokoh@yahoo.com>
2017-12-27 15:16:17 -0500

    Completed Login Page

--- PythonApp/pythonapp.py
+++ PythonApp/pythonapp.py
@@ -1,12 +1,33 @@
+#!/usr/bin/python
 from flask import Flask, flash, redirect \
-    ,render_template, request, session, abort
+    ,render_template, request, session, abort, url_for
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('main'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if session.get('logged_in'):
+        return redirect(url_for('/'))
+
+    if request.method == 'POST':
+        user = request.form['user']
+        password = request.form['password']
+        if user == 'admin' and password == 'password':
+            session['logged_in'] = user
+            return redirect(url_for('main'))
+        else:
+            return render_template('login.html')
     return render_template('login.html')
+
+@app.route('/main',methods=['GET', 'POST'])
+def main():
+    render_template('main.html')
